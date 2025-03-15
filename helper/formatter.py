fd0d2352ee06a18b4731eb10d9b1a6afa85d5b7d
NicholasRui <nrui@caltech.edu>
2025-03-14 18:24:14 -0700

    Fix bug where bool is being interpretted as float

--- helper/formatter.py
+++ helper/formatter.py
@@ -6,12 +6,12 @@ def to_fortran(value):
     Convert from Python to Fortran format
     """
     match value:
+        case bool(): # important: need to check before int, since bool is a subclass of int
+            return '.true.' if value else '.false.'
         case int():
             return f'{value:d}'
         case float():
             return format(value, '.15e').replace('e', 'D')
-        case bool():
-            return '.true.' if value else '.false.'
         case str():
             return value
         case None:
