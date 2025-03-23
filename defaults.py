b28fa318c2c7ea6e821ed4a1396c888c7fff9d1c
NicholasRui <nrui@caltech.edu>
2025-03-22 19:27:09 -0700

    Add more defaults for plot and scatter including cycler

--- defaults.py
+++ defaults.py
@@ -1,9 +1,19 @@
 
 import qol.paths as paths
+from matplotlib import cycler
 
 # default colors
 vis_bg_color = '#edf4ff'
 vis_border_color = 'k'
+
+vis_cycler_colors = []
+vis_cycler_colors.append('xkcd:deep sky blue')
+vis_cycler_colors.append('xkcd:reddish pink')
+vis_cycler_colors.append('xkcd:marigold')
+vis_cycler_colors.append('xkcd:green teal')
+vis_cycler_colors.append('xkcd:magenta')
+
+vis_cycler = cycler(color=vis_cycler_colors)
 
 # default line info
 vis_tick_length = 5
@@ -12,14 +22,22 @@ vis_axis_border_width = 1
 vis_grid_width = 0.2
 vis_grid_alpha = 0.6
 
+vis_plot_linewidth = 3
+
+# default scatter info
+vis_scatter_markersize = 50
+vis_scatter_linewidth = 1.5
+vis_scatter_marker = 'o'
+
 # default font info
 vis_fontpath = f'{paths.qol_path}vis/resources/fonts/HelveticaNeue-01.ttf'
 vis_fontname = 'Helvetica Neue'
 
-vis_title_size = 24
-vis_label_size = 20
+vis_title_size = 20#24
+vis_label_size = 18
 vis_tick_size = 16
 vis_text_size = 14
+vis_legend_size = 14
 
 
 
--- vis/FancyAxes.py
+++ vis/FancyAxes.py
@@ -37,16 +37,19 @@ class FancyAxes(plt.Axes):
         self.xaxis.set_ticks_position('both')
         self.yaxis.set_ticks_position('both')
 
-        # Format spines
+        # format spines
         for spine in self.spines.values():
             spine.set_color(defaults.vis_border_color)
             spine.set_linewidth(defaults.vis_axis_border_width)
             spine.set_zorder(np.inf)
         
-        # Format face and grid
+        # format face and grid
         self.set_facecolor(defaults.vis_bg_color)
         self.grid(c='gray', which='both', alpha=defaults.vis_grid_alpha, linewidth=defaults.vis_grid_width)
         self.set_axisbelow(True) # put grid in background : inf zorder for spines/ticks ensures that they're still on top
+
+        # set color cycler
+        self.set_prop_cycle(defaults.vis_cycler)
 
 
     # Re-define some matplotlib methods to have better defaults
@@ -55,7 +58,6 @@ class FancyAxes(plt.Axes):
         super().set_title(label, fontdict=fontdict, loc=loc, pad=pad, **kwargs)
 
     def set_xlabel(self, xlabel, fontdict=None, labelpad=None, *args, loc=None, **kwargs):
-        print(xlabel)
         kwargs.setdefault('fontsize', defaults.vis_label_size)
         super().set_xlabel(xlabel, fontdict=fontdict, labelpad=labelpad, *args, loc=loc, **kwargs)
 
@@ -63,10 +65,35 @@ class FancyAxes(plt.Axes):
         kwargs.setdefault('fontsize', defaults.vis_label_size)
         super().set_ylabel(ylabel, fontdict=fontdict, labelpad=labelpad, *args, loc=loc, **kwargs)
     
+    def legend(self, *args, **kwargs):
+        kwargs.setdefault('fontsize', defaults.vis_legend_size)
+        super().legend(*args, **kwargs)
+    
+    def text(self, x, y, s, fontdict=None, **kwargs):
+        kwargs.setdefault('fontsize', defaults.vis_text_size)
+        super().text(x, y, s, fontdict=None, **kwargs)
 
+    def plot(self, *args, scalex=True, scaley=True, data=None, **kwargs):
+        # need to do this to handle aliases
+        if 'linewidth' not in kwargs.keys() and \
+                'lw' not in kwargs.keys():
+            kwargs['linewidth'] = defaults.vis_plot_linewidth
+        if 'markersize' not in kwargs.keys() and \
+                'ms' not in kwargs.keys():
+            kwargs['markersize'] = defaults.vis_scatter_markersize
 
+        super().plot(*args, scalex=scalex, scaley=scaley, data=data, **kwargs)
 
+    def scatter(self, x, y, s=None, c=None, *, marker=None, cmap=None, norm=None,
+                vmin=None, vmax=None, alpha=None, linewidths=None, edgecolors=None,
+                colorizer=None, plotnonfinite=False, data=None, **kwargs):
+        marker = marker if marker is not None else defaults.vis_scatter_marker
+        if linewidths is None and \
+                'linewidth' not in kwargs.keys() and \
+                'lw' not in kwargs.keys():
+            linewidths = defaults.vis_scatter_linewidth
+        s = s if s is not None else defaults.vis_scatter_markersize
 
-
-
-
+        super().scatter(x, y, s=s, c=c, marker=marker, cmap=cmap, norm=norm,
+                vmin=vmin, vmax=vmax, alpha=alpha, linewidths=linewidths, edgecolors=edgecolors,
+                colorizer=colorizer, plotnonfinite=plotnonfinite, data=data, **kwargs)
