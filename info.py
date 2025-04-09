cced5ed84edb3ea17297ab576ba075bed33540a8
NicholasRui <nrui@caltech.edu>
2025-04-09 16:58:57 -0700

    Fix bug that prevents getting git commit hash outside qol directory

--- info.py
+++ info.py
@@ -6,6 +6,7 @@ qol_path = qol.__file__.replace('__init__.py', '')
 
 # attempt to get qol commit hash
 try:
-    qol_commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'], stderr=subprocess.DEVNULL).decode('utf-8').strip()
+    qol_commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=qol_path,
+                        stderr=subprocess.DEVNULL).decode('utf-8').strip()
 except:
     qol_commit_hash = 'unknown'
