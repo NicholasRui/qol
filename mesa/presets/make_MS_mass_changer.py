a28bbf9b77e28fcd8f758d8479fb9095afe14cf0
NicholasRui <nrui@caltech.edu>
2025-05-24 18:00:39 -0700

    Add semiconvection as an option for mtglitch runs

--- mesa/presets/make_MS_mass_changer.py
+++ mesa/presets/make_MS_mass_changer.py
@@ -9,6 +9,7 @@ def make_MS_single(root_path, # absolute path in which to write directory
                    enable_pgstar=False,
                    source_sdk=True, # manually activate sdk, since Caltech HPC doesn't seem to like it
                    to_lower_RGB=False, # add task evolving to lower RGB)
+                   alpha_semiconvection=0., # semiconvection parameter
                    ):
     """
     single MS star model for comparison purposes
@@ -16,7 +17,7 @@ def make_MS_single(root_path, # absolute path in which to write directory
     run_name = f'MS{M_in_Msun:.2f}_z{initial_z:.4f}_osf{overshoot_f:.4f}_osf0{overshoot_f0:.4f}'
     run_path = f'{root_path}/{run_name}'
 
-    task_zams_to_mt = helper_MS_mass_changer_zams_to_mt(enable_pgstar=enable_pgstar, M_initial_in_Msun=M_in_Msun, Xcen_accrete=None, initial_z=initial_z, overshoot_f=overshoot_f, overshoot_f0=overshoot_f0)
+    task_zams_to_mt = helper_MS_mass_changer_zams_to_mt(enable_pgstar=enable_pgstar, M_initial_in_Msun=M_in_Msun, Xcen_accrete=None, initial_z=initial_z, overshoot_f=overshoot_f, overshoot_f0=overshoot_f0, alpha_semiconvection=alpha_semiconvection)
 
     # Put it together
     work = MesaWorkingDirectory(run_path=run_path)
@@ -27,7 +28,7 @@ def make_MS_single(root_path, # absolute path in which to write directory
     work.add_task(task_zams_to_mt)
 
     if to_lower_RGB:
-        task_tams_to_lower_rgb = helper_MS_mass_changer_tams_to_lower_rgb(enable_pgstar=enable_pgstar, initial_z=initial_z, last_task='zams_to_mt')
+        task_tams_to_lower_rgb = helper_MS_mass_changer_tams_to_lower_rgb(enable_pgstar=enable_pgstar, initial_z=initial_z, last_task='zams_to_mt', alpha_semiconvection=alpha_semiconvection)
         work.add_task(task_tams_to_lower_rgb)
 
     work.save_directory(slurm_job_name=run_name, grant_perms=True, source_sdk=source_sdk)
@@ -46,15 +47,16 @@ def make_MS_mass_changer(root_path, # absolute path in which to write directory
                          enable_pgstar=False,
                          source_sdk=True, # manually activate sdk, since Caltech HPC doesn't seem to like it
                          to_lower_RGB=False, # add task evolving to lower RGB
+                         alpha_semiconvection=0., # semiconvection parameter
                          ):
     """
     Evolve an MS star for a bit before adding / removing mass from it
     """
-    run_name = f'MS{M_initial_in_Msun:.2f}to{M_final_in_Msun:.2f}atX{Xcen_accrete:.2f}_dM{log_abs_Mdot_accrete:.1f}_z{initial_z:.4f}_osf{overshoot_f:.4f}_osf0{overshoot_f0:.4f}'
+    run_name = f'MS{M_initial_in_Msun:.2f}to{M_final_in_Msun:.2f}atX{Xcen_accrete:.2f}_dM{log_abs_Mdot_accrete:.1f}_z{initial_z:.4f}_osf{overshoot_f:.4f}_osf0{overshoot_f0:.4f}_sc{alpha_semiconvection:.4f}'
     run_path = f'{root_path}/{run_name}'
 
-    task_zams_to_mt = helper_MS_mass_changer_zams_to_mt(enable_pgstar=enable_pgstar, M_initial_in_Msun=M_initial_in_Msun, Xcen_accrete=Xcen_accrete, initial_z=initial_z, overshoot_f=overshoot_f, overshoot_f0=overshoot_f0)
-    task_mt_to_tams = helper_MS_mass_changer_mt_to_tams(enable_pgstar=enable_pgstar, M_initial_in_Msun=M_initial_in_Msun, M_final_in_Msun=M_final_in_Msun, log_abs_Mdot_accrete=log_abs_Mdot_accrete, initial_z=initial_z, overshoot_f=overshoot_f, overshoot_f0=overshoot_f0)
+    task_zams_to_mt = helper_MS_mass_changer_zams_to_mt(enable_pgstar=enable_pgstar, M_initial_in_Msun=M_initial_in_Msun, Xcen_accrete=Xcen_accrete, initial_z=initial_z, overshoot_f=overshoot_f, overshoot_f0=overshoot_f0, alpha_semiconvection=alpha_semiconvection)
+    task_mt_to_tams = helper_MS_mass_changer_mt_to_tams(enable_pgstar=enable_pgstar, M_initial_in_Msun=M_initial_in_Msun, M_final_in_Msun=M_final_in_Msun, log_abs_Mdot_accrete=log_abs_Mdot_accrete, initial_z=initial_z, overshoot_f=overshoot_f, overshoot_f0=overshoot_f0, alpha_semiconvection=alpha_semiconvection)
 
     # Put it together
     work = MesaWorkingDirectory(run_path=run_path)
@@ -67,13 +69,13 @@ def make_MS_mass_changer(root_path, # absolute path in which to write directory
 
     if to_lower_RGB:
         task_tams_to_lower_rgb = helper_MS_mass_changer_tams_to_lower_rgb(enable_pgstar=enable_pgstar, initial_z=initial_z, last_task='mt_to_tams')
-        work.add_task(task_tams_to_lower_rgb)
+        work.add_task(task_tams_to_lower_rgb, alpha_semiconvection=alpha_semiconvection)
 
     work.save_directory(slurm_job_name=run_name, grant_perms=True, source_sdk=source_sdk)
 
     return work
 
-def helper_MS_mass_changer_zams_to_mt(enable_pgstar, M_initial_in_Msun, Xcen_accrete, initial_z, overshoot_f, overshoot_f0):
+def helper_MS_mass_changer_zams_to_mt(enable_pgstar, M_initial_in_Msun, Xcen_accrete, initial_z, overshoot_f, overshoot_f0, alpha_semiconvection):
     """
     Evolve up to the mass transfer time
     """    
@@ -91,6 +93,8 @@ def helper_MS_mass_changer_zams_to_mt(enable_pgstar, M_initial_in_Msun, Xcen_acc
     inlist.initial_z(initial_z)
     inlist.set_Zbase(initial_z)
 
+    inlist.alpha_semiconvection(alpha_semiconvection)
+
     # Termination conditions
     inlist.stop_at_phase_TAMS()
     if Xcen_accrete is not None:
@@ -105,7 +109,7 @@ def helper_MS_mass_changer_zams_to_mt(enable_pgstar, M_initial_in_Msun, Xcen_acc
 
     return inlist
 
-def helper_MS_mass_changer_mt_to_tams(enable_pgstar, M_initial_in_Msun, M_final_in_Msun, log_abs_Mdot_accrete, initial_z, overshoot_f, overshoot_f0):
+def helper_MS_mass_changer_mt_to_tams(enable_pgstar, M_initial_in_Msun, M_final_in_Msun, log_abs_Mdot_accrete, initial_z, overshoot_f, overshoot_f0, alpha_semiconvection):
     """
     Perform mass transfer and evolve to TAMS
     """
@@ -118,6 +122,8 @@ def helper_MS_mass_changer_mt_to_tams(enable_pgstar, M_initial_in_Msun, M_final_
     # Initial conditions
     inlist.load_model('zams_to_mt.mod')
     inlist.set_Zbase(initial_z)
+
+    inlist.alpha_semiconvection(alpha_semiconvection)
 
     # Write GYRE
     inlist.write_gyre_data_with_profile()
@@ -139,7 +145,7 @@ def helper_MS_mass_changer_mt_to_tams(enable_pgstar, M_initial_in_Msun, M_final_
 
     return inlist
 
-def helper_MS_mass_changer_tams_to_lower_rgb(enable_pgstar, initial_z, last_task):
+def helper_MS_mass_changer_tams_to_lower_rgb(enable_pgstar, initial_z, last_task, alpha_semiconvection):
     inlist = MesaInlist('tams_to_lower_rgb')
     if enable_pgstar:
         inlist.enable_pgstar()
@@ -155,6 +161,8 @@ def helper_MS_mass_changer_tams_to_lower_rgb(enable_pgstar, initial_z, last_task
             inlist.load_model('mt_to_tams.mod')
         case _:
             raise ValueError(f"invalid last_task {last_task}: must be either 'zams_to_mt' or 'mt_to_tams'")
+
+    inlist.alpha_semiconvection(alpha_semiconvection)
 
     # Write GYRE
     inlist.write_gyre_data_with_profile()
