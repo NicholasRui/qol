f55003ccb94004a36e937bfc9ae462825d47e983
NicholasRui <nrui@caltech.edu>
2025-06-19 15:16:45 -0700

    Fix another bug related to call_create_shell_burning_remnant.py

--- mesa/templates/scripts/call_create_shell_burning_remnant.py
+++ mesa/templates/scripts/call_create_shell_burning_remnant.py
@@ -7,6 +7,11 @@ core_mod_fname = sys.argv[1]
 env_mod_fname = sys.argv[2]
 write_mod_fname = sys.argv[3]
 
+# Append 'data/' to the front of all of these
+core_mod_fname = f'data/{core_mod_fname}'
+env_mod_fname = f'data/{env_mod_fname}'
+write_mod_fname = f'data/{write_mod_fname}'
+
 interface_setting = 'excise'
 readjust_setting = 'change_m'
 
