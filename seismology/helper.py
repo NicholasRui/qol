1c6a90fc79fead24e9f147db03d6a8df4789fc41
NicholasRui <nicholas.z.rui@gmail.com>
2025-11-08 21:15:37 -0500

    Seismology: add method which conveniently computes dimensionful asymmetry parameter

--- seismology/helper.py
+++ seismology/helper.py
@@ -1,7 +1,11 @@
 """
 Helper functions for Seismology class and associated attributes
+
+Helpers are those that don't depend on the state of the Seismology class.
 """
+
 import numpy as np
+from math import gcd
 
 # TODO: add methods to do additional conversions
 
@@ -88,5 +92,37 @@ def get_magnetic_aasym(l, m1, m2, m3, asym_ref='RFH+25'):
     
     return aasym
 
+def compute_Δasym(l, m1, m2, m3, ν1, ν2, ν3):
+    """
+    Compute the dimensionful asymmetry parameter as described by RFH+25, Appendix A.2.
 
+    This takes linear combinations of three frequencies ν1, ν2, ν3 defined as
+    Δasym = c1 ν1 + c2 ν2 + c3 ν3
 
+    such that both
+    c1 + c2 + c3 = 0
+    c1 m1 + c2 m2 + c3 m3 = 0
+
+    We choose c1, c2, c3 such that the integers are the lowest possible, and at most one is negative
+    """
+    assert isinstance(l, int) and isinstance(m1, int) and isinstance(m2, int) and isinstance(m3, int)
+    assert (np.abs(m1) <= l) and (np.abs(m2) <= l) and (np.abs(m3) <= l)
+    assert len(set([m1, m2, m3])) == 3 # must be distinct
+
+    # Sort frequencies by m
+    m_sorted, ν_sorted = zip(*sorted(zip((m1, m2, m3), (ν1, ν2, ν3))))
+    m1, m2, m3 = m_sorted
+    ν1, ν2, ν3 = ν_sorted
+
+    # Compute coefficients
+    # This is naturally preserved by the choice of coefficients:
+    #  c1 = m3 - m2, c2 = m1 - m3, c3 = m2 - m1
+    # and we will simply divide by the GCD to get the smallest integers
+    c1, c2, c3 = m3 - m2, m1 - m3, m2 - m1
+    g = gcd(gcd(abs(c1), abs(c2)), abs(c3))
+    c1, c2, c3 = c1 // g, c2 // g, c3 // g
+
+    # Compute Δasym
+    Δasym = c1 * ν1 + c2 * ν2 + c3 * ν3
+
+    return Δasym
