fa7855f949f1f2663f97be817e03a92a90af0cfa
NicholasRui <nicholas.z.rui@gmail.com>
2025-11-08 20:55:59 -0500

    Add method for magnetic asymmetry parameter

--- seismology/helper.py
+++ seismology/helper.py
@@ -35,8 +35,58 @@ def get_magnetic_acrit(l, m=None, acrit_ref='F+15'):
         case 'RF+23':
             raise NotImplementedError('acrit_ref for RF+23 option not implemented yet') # TODO
         case _:
-            raise ValueError(f'ac_ref is not valid: {acrit_ref}')
+            raise ValueError(f'acrit_ref is not valid: {acrit_ref}')
 
     return acrit
+
+def get_magnetic_aasym(l, m1, m2, m3, asym_ref='RFH+25'):
+    """
+    Gets "good guess" for uncertain magnetic asymmetry, called 'a' in Li+2022 and other works for dipole modes.
+    Definition for a_asym for arbitrary (l, m) triplets is given in RFH+25.
+
+    asym_ref: sets "good guess" for uncertain dimensionless asymmetry parameter,
+        'RFH+25': Rui, Fuller, Hermes+2025 definition, which assumes relative asymmetry parameters ~ oblique dipole
+                  averaged over all possible angles in the RMS sense.
+    """
+    assert isinstance(l, int) and isinstance(m1, int) and isinstance(m2, int) and isinstance(m3, int)
+    assert (np.abs(m1) <= l) and (np.abs(m2) <= l) and (np.abs(m3) <= l)
+    assert len(set([m1, m2, m3])) == 3 # must be distinct
+
+    # Flip all signs if majority are negative, since asymmetry is invariant under this
+    if (m1 < 0) + (m2 < 0) + (m3 < 0) >= 2:
+        m1, m2, m3 = -m1, -m2, -m3
+
+    # Use set type to ignore order of (m1, m2, m3)
+    match asym_ref:
+        case 'RFH+25':
+            P2cosβ_rms = np.sqrt(2/5) # RMS value of cosβ over all dipole obliquity angles
+
+            match l:
+                case 1: # DIPOLE
+                    match {m1, m2, m3}:
+                        case (-1, 0, 1):
+                            aasym = P2cosβ_rms * 2/5
+                        case _:
+                            raise ValueError(f'(m1, m2, m3) is not valid: {(m1, m2, m3)}')
+                
+                case 2: # QUADRUPOLE
+                    match {m1, m2, m3}:
+                        case (-1, 0, 1) | (0, 1, 2):
+                            aasym = P2cosβ_rms * -2/35
+                        case (-2, 0, 1) | (-1, 0, 2) | (-1, 1, 2):
+                            aasym = P2cosβ_rms * -6/35
+                        case (-2, 1, 2):
+                            aasym = P2cosβ_rms * -12/35
+                        case (-2, 0, 2):
+                            aasym = P2cosβ_rms * -8/35
+                
+                case _:
+                    raise ValueError(f'asym_ref is not valid: {asym_ref}')
+        
+        case _:
+            raise ValueError(f'asym_ref is not valid: {asym_ref}')
+    
+    return aasym
+
 
 
