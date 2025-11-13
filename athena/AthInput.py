fcdac81675502d68549e53d2c0e3833d09b61d71
NicholasRui <nicholas.z.rui@gmail.com>
2025-11-13 15:49:29 -0500

    Add AthInput class for helping automatically generate AthInput files for Athena++ runs

---
+++ athena/AthInput.py
@@ -0,0 +1,73 @@
+"""
+Class which helps initialize an athinput file (input file for Athena++).
+"""
+import numpy as np
+
+import warnings
+
+
+class AthArg:
+    """
+    Helper object which stores individual arguments, in the following way:
+
+    <block>
+    name = value   # comment
+    """
+    def __init__(self, block, name, value, comment=None):
+        self.block = block
+        self.name = name
+        self.value = value
+        self.comment = comment
+
+
+class AthInput:
+    def __init__(self):
+        self.athargs = []
+
+    def add_argument(self, block, name, value, comment=None):
+        """
+        Add an argument
+        """
+        self.athargs.append(AthArg(block=block, name=name, value=value, comment=comment))
+            
+    def get_blocks(self):
+        blocks = list(set([arg.block for arg in self.arguments]))
+
+        # Sort it in the following way:
+        #  1. elements in some named list
+        #  2. output blocks
+        #  3. everything else
+        first_blocks = ['comment', 'job', 'mesh', 'time', 'problem']
+        order_map = {block: ii for ii, block in enumerate(first_blocks)}
+
+        sort_func = lambda block: (0, order_map[block]) if block in first_blocks else (1, block) if block.startswith('output') else (2, block)
+        blocks = sorted(blocks, key=sort_func)
+
+        return blocks
+
+
+    def save(self, fname):
+        """
+        Save AthInput file at filename
+        """
+        blocks = self.get_blocks()
+
+        outstr = ''
+        for block in blocks:
+            outstr += f'<{block}>\n'
+
+            # get arguments within block
+            athargs_block = sorted([atharg for atharg in self.athargs if atharg.block == block])
+            atharg_names = [atharg.name for atharg in athargs_block]
+            atharg_comments = [atharg.comment for atharg in athargs_block]
+
+            # add each argument to outstr, making sure equal and comment signs are aligned with each other
+            max_len_name = max([len(name) for name in atharg_names])
+            atharg_strs = [f'{atharg.name.ljust(max_len_name)} = {str(atharg.value)}' for atharg in athargs_block]
+            max_len_atharg_str = max([len(atharg_str) for atharg_str in atharg_strs])
+            atharg_strs = [f'{atharg_str.ljust(max_len_atharg_str)} # {atharg_comments[ii]}\n' if atharg_comments[ii] is not None else f'{atharg_str}\n' for ii, atharg_str in enumerate(atharg_strs)]
+
+            outstr += ''.join(atharg_strs) + '\n'
+
+        with open(fname, 'w') as f:
+            f.write(outstr)
